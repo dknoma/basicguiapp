267d1bae4f97122a8f264a3d36c8f9e911bb67e6
dknoma <djknoma@gmail.com>
2019-02-06 22:10:51 -0800

    Basic Python GUI that formats the text in the input box to change all URLs to [url=<URL>]INSERT TEXT HERE[/url], and removes all Discord Timestamp texts from each line before a users name.

--- SMWCInterviewFormatter.py
+++ SMWCInterviewFormatter.py
@@ -110,3 +110,32 @@ text = timestampPattern.sub(formatter.tsformat, text)
 # Format all URLs to be SMWC post ready
 out = urlPattern.sub(formatter.urlformat, text)
 print(out)
+
+
+# Text in kinter are made with Label
+# Format: Label(<window>, text="Text")
+# labelOne = Label(mainWindow, text="HELLO THIS IS A SIMPLE PYTHON GUI TEST")
+# Pack puts label in first available space
+# labelOne.pack()
+
+# Pack layout
+# topFrame = Frame(mainWindow)
+# topFrame.pack()
+# bottomFrame = Frame(mainWindow)
+# # Packs the frame container on the bottom of the window
+# bottomFrame.pack(side=BOTTOM)
+#
+# # Settings
+# title = Label(topFrame, text="GUI Test", bg="grey", fg="black")
+# # Button(whichFrame, text="button text", fg="color")
+# button1 = Button(topFrame, text="Button 1", fg="blue")
+# button2 = Button(topFrame, text="Button 2", fg="green")
+# button3 = Button(bottomFrame, text="Button 3", fg="red")
+# button4 = Button(bottomFrame, text="Button 4", fg="purple")
+#
+# # Pack settings
+# title.pack(fill=X)
+# button1.pack(side=LEFT)
+# button2.pack(side=RIGHT)
+# button3.pack(side=LEFT)
+# button4.pack(side=RIGHT)
--- guitest.py
+++ guitest.py
@@ -1,19 +1,52 @@
 from tkinter import *
 # Import all tkinter classes
 
+
+class HTMLFormatter:
+    def __init__(self):
+        self.url = ''
+
+    def tsformat(self, mathobject):
+        return ''
+
+    def urlformat(self, matchobject):
+        self.url = '[url=' + matchobject.group(0) + ']INSERT TEXT HERE[/url]'
+        return str(self.url)
+
+
+def formatText(inputText):
+    print("formatting...")
+    # HTTP regex
+    urlRegex = r'(?P<url>https?://[^\s]+)'
+    # Discord timestamp regex
+    tsRegex = r'(\[\d+:\d+\s(AM|PM)\]\s)'
+    # HTML Discord Interview Formatter
+    formatter = HTMLFormatter()
+    urlPattern = re.compile(urlRegex)
+    timestampPattern = re.compile(tsRegex)
+    # Remove all timestamps
+    inputText = timestampPattern.sub(formatter.tsformat, inputText)
+    # Format all URLs to be SMWC post ready
+    out = urlPattern.sub(formatter.urlformat, inputText)
+    print(out)
+    root.clipboard_clear()
+    root.clipboard_append(out)
+
+
 # Create new window
 # End with mainloop() to keep window persistent
 root = Tk()
+textToCopy = ''
 
 # title = Label(root, text="GUI Tests", bg="grey", fg="white")
-title = Label(root, text="GUI Tests", bg="grey")
-inputLabel = Label(root, text="Text Input", bg="grey")
+title = Label(root, text="Text Input")
 inputEntry = Text(root)
 formatButton = Button(root, text="Format text", fg="grey")
+# Bind a function to the button that activates on left click(<Button-1>)
+formatButton.bind("<Button-1>", lambda inputText: formatText(inputEntry.get("1.0", END)))
 
 # sticky=n, e, s, or w for align inside the grid
 title.grid(row=0, column=1)
-inputLabel.grid(row=1, column=0)
 inputEntry.grid(row=1, column=1, sticky=N+S+E+W)
 formatButton.grid(row=2, column=1, sticky=N)
 
@@ -22,31 +55,3 @@ root.columnconfigure(1, weight=1)
 
 # Allows window to persist
 root.mainloop()
-
-# Text in kinter are made with Label
-# Format: Label(<window>, text="Text")
-# labelOne = Label(mainWindow, text="HELLO THIS IS A SIMPLE PYTHON GUI TEST")
-# Pack puts label in first available space
-# labelOne.pack()
-
-# Pack layout
-# topFrame = Frame(mainWindow)
-# topFrame.pack()
-# bottomFrame = Frame(mainWindow)
-# # Packs the frame container on the bottom of the window
-# bottomFrame.pack(side=BOTTOM)
-#
-# # Settings
-# title = Label(topFrame, text="GUI Test", bg="grey", fg="black")
-# # Button(whichFrame, text="button text", fg="color")
-# button1 = Button(topFrame, text="Button 1", fg="blue")
-# button2 = Button(topFrame, text="Button 2", fg="green")
-# button3 = Button(bottomFrame, text="Button 3", fg="red")
-# button4 = Button(bottomFrame, text="Button 4", fg="purple")
-#
-# # Pack settings
-# title.pack(fill=X)
-# button1.pack(side=LEFT)
-# button2.pack(side=RIGHT)
-# button3.pack(side=LEFT)
-# button4.pack(side=RIGHT)
