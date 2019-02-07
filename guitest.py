4b972a5c6418ac3f305d89afdbafe36c9dc0075d
dknoma <djknoma@gmail.com>
2019-02-06 18:48:35 -0800

    Adding GUI testing files

---
+++ guitest.py
@@ -0,0 +1,26 @@
+from tkinter import *
+# Import all tkinter classes
+
+# Create new window
+mainWindow = Tk()
+# Text in kinter are made with Label
+# Format: Label(<window>, text="Text")
+labelOne = Label(mainWindow, text="HELLOW THIS IS A SIMPLE PYTHON GUI TEST")
+# Pack puts label in first available space
+labelOne.pack()
+# Allows window to persist
+mainWindow.mainloop()
+
+
+
+
+
+
+
+
+
+
+
+
+
+
